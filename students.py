733161601808173d22ee707a52470a153f941245
Jen <jennifer.stemkowski@gmail.com>
2024-03-30 17:41:18 -0400

    Create cursor + table

--- students.py
+++ students.py
@@ -1,3 +1,9 @@
 import psycopg2
 
-con = psycopg2.connect(dbname='studentdb', user='postgres', password='Coopercooper12', host='localhost', port='5432')
+conn = psycopg2.connect(dbname='studentdb', user='postgres', password='Coopercooper12', host='localhost', port='5432')
+cur = conn.cursor()
+
+cur.execute("create table students(student_id serial primary key, name text, address text, age int, number text);")
+
+conn.commit()
+conn.close()
