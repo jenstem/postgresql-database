ecbc9555a9574d740920b1b955b4008abe5e28c6
Jen <jennifer.stemkowski@gmail.com>
2024-03-30 18:49:53 -0400

    Revise func update_data for single field update

--- students.py
+++ students.py
@@ -23,13 +23,28 @@ def insert_data():
 
 def update_data():
     student_id = input("Enter student id to be updated: ")
-    name = input("Enter name: ")
-    address = input("Enter address: ")
-    age = input("Enter age: ")
-    number = input("Enter phone number: ")
     conn = psycopg2.connect(dbname='studentdb', user='postgres', password='Coopercooper12', host='localhost', port='5432')
     cur = conn.cursor()
-    cur.execute("update students set name=%s , address=%s , age=%s , number=%s where student_id=%s", (name, address, age, number, student_id))
+    fields = {
+        "1": ("name", "Enter new name: "),
+        "2": ("address", "Enter new address: "),
+        "3": ("age", "Enter new age: "),
+        "4": ("number", "Enter new phone number: "),
+    }
+    print("Select field to update: ")
+    for key in fields:
+        print(f"{key}:{fields[key][0]}")
+    field_choice = input("Enter field number to be updated: ")
+
+    if field_choice in fields:
+        field_name, prompt = fields[field_choice]
+        new_value = input(prompt)
+        sql = f"update students set {field_name}=%s where student_id=%s"
+        cur.execute(sql, (new_value, student_id))
+        print(f"{field_name} updated successfully")
+    else:
+        print("Invalid choice, please try again")
+
     conn.commit()
     conn.close()
 
