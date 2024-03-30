b7b1851a71689c67106ba7426a7450ca012e15e7
Jen <jennifer.stemkowski@gmail.com>
2024-03-30 17:59:53 -0400

    Create func insert_data

--- students.py
+++ students.py
@@ -7,3 +7,18 @@ def create_table():
     cur.execute("create table students(student_id serial primary key, name text, address text, age int, number text);")
     conn.commit()
     conn.close()
+
+
+def insert_data():
+    name = input("Enter name: ")
+    address = input("Enter address: ")
+    age = input("Enter age: ")
+    number = input("Enter phone number: ")
+    conn = psycopg2.connect(dbname='studentdb', user='postgres', password='Coopercooper12', host='localhost', port='5432')
+    cur = conn.cursor()
+    cur.execute("insert into students(name, address, age, number) values (%s, %s, %s, %s)", (name, address, age, number))
+    print("Data added successfully")
+    conn.commit()
+    conn.close()
+
+insert_data()
