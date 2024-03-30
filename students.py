fe7ac721293765d1dd6789c1b6fb8001edee35c4
Jen <jennifer.stemkowski@gmail.com>
2024-03-30 18:14:24 -0400

    Create func update_data

--- students.py
+++ students.py
@@ -21,4 +21,17 @@ def insert_data():
     conn.commit()
     conn.close()
 
-insert_data()
+def update_data():
+    student_id = input("Enter student id to be updated: ")
+    name = input("Enter name: ")
+    address = input("Enter address: ")
+    age = input("Enter age: ")
+    number = input("Enter phone number: ")
+    conn = psycopg2.connect(dbname='studentdb', user='postgres', password='Coopercooper12', host='localhost', port='5432')
+    cur = conn.cursor()
+    cur.execute("update students set name=%s , address=%s , age=%s , number=%s where student_id=%s", (name, address, age, number, student_id))
+    conn.commit()
+    conn.close()
+
+# insert_data()
+update_data()
