8e05ccda90fecafe821f8516175a53d09a47db39
Jen <jennifer.stemkowski@gmail.com>
2024-04-01 13:42:10 -0400

    Define columns + label headings

--- input.py
+++ input.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 root = Tk()
 root.title("Student Management System")
 
-
+# Frame
 frame = LabelFrame(root, text="Student Data")
 frame.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
@@ -21,7 +21,7 @@ age_entry = Entry(frame).grid(row=2, column=1, pady=2, sticky="ew")
 Label(frame, text="Phone Number: ").grid(row=3, column=0, padx=2, sticky="w")
 phone_entry = Entry(frame).grid(row=3, column=1, pady=2, sticky="ew")
 
-
+# Buttons
 button_frame = Frame(root)
 button_frame.grid(row=1, column=0, padx=10, pady=5, sticky="ew")
 
@@ -30,7 +30,7 @@ Button(button_frame, text="Add Data").grid(row=0, column=1, padx=5)
 Button(button_frame, text="Update Data").grid(row=0, column=2, padx=5)
 Button(button_frame, text="Delete Data").grid(row=0, column=3, padx=5)
 
-
+# Treeview
 tree_frame = Frame(root)
 tree_frame.grid(row=2, column=0, padx=10, sticky="nsew")
 
@@ -40,6 +40,22 @@ tree_scroll.pack(side=RIGHT, fill=Y)
 tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="browse")
 tree.pack()
 tree_scroll.config(command=tree.yview)
+
+# Columns
+tree['columns']=("student_id", "name", "address", "age", "number")
+tree.column("#0", width=0, stretch=NO)
+tree.column("student_id", anchor=CENTER, width=80)
+tree.column("name", anchor=CENTER, width=120)
+tree.column("address", anchor=CENTER, width=120)
+tree.column("age", anchor=CENTER, width=50)
+tree.column("number", anchor=CENTER, width=120)
+
+# Column Headings
+tree.heading("student_id", text="ID", anchor=CENTER)
+tree.heading("name", text="Name", anchor=CENTER)
+tree.heading("address", text="Address", anchor=CENTER)
+tree.heading("age", text="Age", anchor=CENTER)
+tree.heading("number", text="Phone Number", anchor=CENTER)
 
 
 root.mainloop()
