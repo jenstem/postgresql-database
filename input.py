ac26df59e7b37cdb987a2f19a1d717459da56fff
Jen <jennifer.stemkowski@gmail.com>
2024-04-01 18:25:43 -0400

    Debug GET error

--- input.py
+++ input.py
@@ -11,7 +11,7 @@ def run_query(query, parameters=()):
     query_result = None
     try:
         cur.execute(query, parameters)
-        if query.lower().startswith('select'):
+        if query.lower().startswith("select"):
             query_result = cur.fetchall()
         conn.commit()
     except psycopg2.Error as e:
@@ -51,33 +51,57 @@ def delete_data():
     refresh_treeview()
 
 
+# Update Data
+def update_data():
+    selected_item = tree.selection()[0]
+    student_id = tree.item(selected_item)['values'][0]
+    query = "update students set name=%s, address=%s, age=%s, number=%s where student_id=%s"
+    parameters = (name_entry.get(), address_entry.get(), age_entry.get(), phone_entry.get(), student_id)
+    run_query(query, parameters)
+    messagebox.showinfo("Information", "Data updated successfully")
+    refresh_treeview()
+
+
+# Create Table
+def create_table():
+    # only create table if it doesn't already exist
+    query = "create table if not exists students(student_id serial primary key, name text, address text, age int, number text);"
+    run_query(query)
+    messagebox.showinfo("Information", "Table created successfully")
+    refresh_treeview()
+
+
 root = Tk()
 root.title("Student Management System")
 
 
-# Frame
+# Fields & Labels
 frame = LabelFrame(root, text="Student Data")
 frame.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-Label(frame, text="Name: ").grid(row=0, column=0, padx=2, sticky="w")
-name_entry = Entry(frame).grid(row=0, column=1, pady=2, sticky="ew")
+Label(frame, text="Name: ").grid(row=0, column=0, pady=2, sticky="w")
+name_entry = Entry(frame)
+name_entry.grid(row=0, column=1, pady=2, sticky="ew")
 
 Label(frame, text="Address: ").grid(row=1, column=0, padx=2, sticky="w")
-address_entry = Entry(frame).grid(row=1, column=1, pady=2, sticky="ew")
+address_entry = Entry(frame)
+address_entry.grid(row=1, column=1, pady=2, sticky="ew")
 
 Label(frame, text="Age: ").grid(row=2, column=0, padx=2, sticky="w")
-age_entry = Entry(frame).grid(row=2, column=1, pady=2, sticky="ew")
+age_entry = Entry(frame)
+age_entry.grid(row=2, column=1, pady=2, sticky="ew")
 
 Label(frame, text="Phone Number: ").grid(row=3, column=0, padx=2, sticky="w")
-phone_entry = Entry(frame).grid(row=3, column=1, pady=2, sticky="ew")
+phone_entry = Entry(frame)
+phone_entry.grid(row=3, column=1, pady=2, sticky="ew")
 
 # Buttons
 button_frame = Frame(root)
 button_frame.grid(row=1, column=0, padx=10, pady=5, sticky="ew")
 
-Button(button_frame, text="Create Table").grid(row=0, column=0, padx=5)
+Button(button_frame, text="Create Table", command=create_table).grid(row=0, column=0, padx=5)
 Button(button_frame, text="Add Data", command=insert_data).grid(row=0, column=1, padx=5)
-Button(button_frame, text="Update Data").grid(row=0, column=2, padx=5)
+Button(button_frame, text="Update Data", command=update_data).grid(row=0, column=2, padx=5)
 Button(button_frame, text="Delete Data", command=delete_data).grid(row=0, column=3, padx=5)
 
 # Treeview
@@ -92,7 +116,7 @@ tree.pack()
 tree_scroll.config(command=tree.yview)
 
 # Columns
-tree['columns']=("student_id", "name", "address", "age", "number")
+tree['columns'] = ("student_id", "name", "address", "age", "number")
 tree.column("#0", width=0, stretch=NO)
 tree.column("student_id", anchor=CENTER, width=80)
 tree.column("name", anchor=CENTER, width=120)
