34ada1d0a69cdbafad1484af37fbe8915272d6ab
Jen <jennifer.stemkowski@gmail.com>
2024-04-01 11:54:09 -0400

    Add buttons for menu options

--- input.py
+++ input.py
@@ -8,7 +8,6 @@ root.title("Student Management System")
 frame = LabelFrame(root, text="Student Data")
 frame.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-
 Label(frame, text="Name: ").grid(row=0, column=0, padx=2, sticky="w")
 name_entry = Entry(frame).grid(row=0, column=1, pady=2, sticky="ew")
 
@@ -20,6 +19,15 @@ age_entry = Entry(frame).grid(row=2, column=1, pady=2, sticky="ew")
 
 Label(frame, text="Phone Number: ").grid(row=3, column=0, padx=2, sticky="w")
 phone_entry = Entry(frame).grid(row=3, column=1, pady=2, sticky="ew")
+
+
+button_frame = Frame(root)
+button_frame.grid(row=1, column=0, padx=10, pady=5, sticky="ew")
+
+Button(button_frame, text="Create Table").grid(row=0, column=0, padx=5)
+Button(button_frame, text="Add Data").grid(row=0, column=1, padx=5)
+Button(button_frame, text="Update Data").grid(row=0, column=2, padx=5)
+Button(button_frame, text="Delete Data").grid(row=0, column=3, padx=5)
 
 
 root.mainloop()
