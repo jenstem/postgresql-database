34d403ad44c7a417f3db1ef43a486f63dee08672
Jen <jennifer.stemkowski@gmail.com>
2024-04-01 13:32:05 -0400

    Create Treeview + ScrollBar for displaying data

--- input.py
+++ input.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 
 root = Tk()
@@ -28,6 +29,17 @@ Button(button_frame, text="Create Table").grid(row=0, column=0, padx=5)
 Button(button_frame, text="Add Data").grid(row=0, column=1, padx=5)
 Button(button_frame, text="Update Data").grid(row=0, column=2, padx=5)
 Button(button_frame, text="Delete Data").grid(row=0, column=3, padx=5)
+
+
+tree_frame = Frame(root)
+tree_frame.grid(row=2, column=0, padx=10, sticky="nsew")
+
+tree_scroll = Scrollbar(tree_frame)
+tree_scroll.pack(side=RIGHT, fill=Y)
+
+tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="browse")
+tree.pack()
+tree_scroll.config(command=tree.yview)
 
 
 root.mainloop()
