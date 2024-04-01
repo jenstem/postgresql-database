7080e6bccadaa88abf59877db8cc842a6ee31dea
Jen <jennifer.stemkowski@gmail.com>
2024-04-01 11:21:05 -0400

    Create func read_data + add user menu

--- students.py
+++ students.py
@@ -24,21 +24,13 @@ def insert_data():
 
 def delete_data():
     student_id = input("Enter student id to be deleted: ")
-    # connect to the database
     conn = psycopg2.connect(dbname='studentdb', user='postgres', password='Coopercooper12', host='localhost', port='5432')
     cur = conn.cursor()
-    # confirm that student exists by making a query
-    # need to put a comma after student_id to make it a tuple - even with a single argument
     cur.execute("select * from students where student_id=%s", (student_id,))
-    # this will return an entire row of the student with the given student_id
     student = cur.fetchone()
-    # does student row exist?
     if student:
-        # if student exists, delete the student
         print(f"Student to be delete: ID: {student[0]}, Name: {student[1]}, Address: {student[2]}, Age: {student[3]}, Number: {student[4]}")
-        # confirm deletion
         choice = input("Are you sure you want to delete this student? (y/n): ")
-        # make answer lowercase
         if choice.lower() == "y":
             cur.execute("delete from students where student_id=%s", (student_id,))
             print("Student deleted successfully")
@@ -46,7 +38,6 @@ def delete_data():
             print("Student not deleted")
     else:
         print("Student not found")
-    # commit and close connection
     conn.commit()
     conn.close()
 
@@ -78,6 +69,37 @@ def update_data():
     conn.commit()
     conn.close()
 
-# insert_data()
-# update_data()
-delete_data()
+
+def read_data():
+    conn = psycopg2.connect(dbname='studentdb', user='postgres', password='Coopercooper12', host='localhost', port='5432')
+    cur = conn.cursor()
+    cur.execute("select * from students;")
+    students = cur.fetchall()
+    for student in students:
+        print(f"ID: {student[0]}, Name: {student[1]}, Address: {student[2]}, Age: {student[3]}, Number: {student[4]}")
+    conn.close()
+
+
+while True:
+    print("\n Welcome to the student database management system")
+    print("1. Create table")
+    print("2. Insert Data")
+    print("3. Read Data")
+    print("4. Update Data")
+    print("5. Delete Data")
+    print("6. Exit")
+    choice = input("Enter your choice: (1 - 6): ")
+    if choice == '1':
+        create_table()
+    elif choice == '2':
+        insert_data()
+    elif choice == '3':
+        read_data()
+    elif choice == '4':
+        update_data()
+    elif choice == '5':
+        delete_data()
+    elif choice == '6':
+        break
+    else:
+        print("Invalid choice, please try again")
